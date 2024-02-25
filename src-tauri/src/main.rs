9eaad7055cb82a7c95b1ea4c7792dfcc5da04f67
이정진 <47263683+jung-jin-lee@users.noreply.github.com>
2024-02-25 16:52:43 +0900

    feat: 대화방/메시지 api 추가 (#5)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,9 +10,50 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create conversations table",
+            sql: "CREATE TABLE conversations (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                createdAt NUMERIC DEFAULT CURRENT_TIMESTAMP,
+                updatedAt NUMERIC 
+            );
+
+            CREATE TRIGGER trigger_conversations_updated_at
+            AFTER UPDATE ON conversations
+            FOR EACH ROW
+            BEGIN
+                UPDATE conversations
+                SET updatedAt = CURRENT_TIMESTAMP
+                WHERE id = NEW.id;
+            END;
+            
+            ",
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: "create messages table",
+            sql: "CREATE TABLE messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                conversationId INTEGER NOT NULL,
+                model TEXT DEFAULT '',
+                role TEXT  DEFAULT '',
+                content TEXT NOT NULL,
+                createdAt NUMERIC DEFAULT CURRENT_TIMESTAMP
+            );
+            
+            ",
+            kind: MigrationKind::Up,
+        },
+    ];
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_sql::Builder::default().add_migrations("sqlite::magic.db", migrations).build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
