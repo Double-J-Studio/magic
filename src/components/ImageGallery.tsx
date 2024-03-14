cfd2df292cf2f5067a5a208298705f347005700f
WOOJINLEEdev <lwj3794@naver.com>
2024-03-14 15:17:54 +0900

    feat: 이미지 갤러리에서 이미지 로딩 중 skeleton ui 적용

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,10 +1,44 @@
-import { Images } from "@/pages/SettingPage";
+import { useEffect } from "react";
 
-interface ImageGalleryProps {
-  images?: Images;
-}
+import { readImage, readImages } from "@/utils/tauri/file";
 
-const ImageGallery = ({ images }: ImageGalleryProps) => {
+import { Skeleton } from "@/components/ui/skeleton";
+
+import useImageGalleryStore, { Images } from "@/state/useImageGalleryStore";
+
+const ImageGallery = () => {
+  const { images, setImages } = useImageGalleryStore();
+
+  useEffect(() => {
+    async function uint8ArrayToBlobUrl() {
+      const imgs = await readImages();
+      const imagesObj: Images = {};
+
+      imgs.map(async (img) => {
+        imagesObj[img.path] = { loading: true, blobUrl: "" };
+      });
+      setImages(imagesObj);
+
+      await Promise.allSettled(
+        imgs.map(async (img) => {
+          const res = await readImage(img.path);
+
+          const blob = new Blob([res], { type: "image/jpeg" });
+          const blobUrl = URL.createObjectURL(blob);
+
+          imagesObj[img.path] = {
+            loading: false,
+            blobUrl: blobUrl,
+          };
+          setImages(imagesObj);
+        })
+      );
+    }
+
+    uint8ArrayToBlobUrl();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="w-full min-h-screen p-5">
       <ul className="grid grid-cols-4 gap-2">
@@ -12,11 +46,15 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
           Object.keys(images).map((image, i) => {
             return (
               <li key={`${image}_${i}`}>
-                <img
-                  src={images[image].blobUrl}
-                  alt=""
-                  className="rounded-md"
-                />
+                {images[image].loading ? (
+                  <Skeleton className="w-full min-w-[120px] h-full min-h-[120px] rounded-xl" />
+                ) : (
+                  <img
+                    src={images[image].blobUrl}
+                    alt=""
+                    className="rounded-md"
+                  />
+                )}
               </li>
             );
           })}
