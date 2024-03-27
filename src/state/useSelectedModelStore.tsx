47be930e9ab89477de3fefda9e30d53ff9e759ca
WOOJINLEEdev <79506033+WOOJINLEEdev@users.noreply.github.com>
2024-03-27 11:50:59 +0900

    feat: local storage에 선택한 모델 저장 (#71)

--- src/state/useSelectedModelStore.tsx
+++ src/state/useSelectedModelStore.tsx
@@ -1,17 +1,23 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface UseSelectedModelStoreProps {
-  // model: "gpt-3.5-turbo" | "gpt-4-turbo-preview";
   model: string;
   setModel: (model: string) => void;
 }
 
-const useSelectedModelStore = create<UseSelectedModelStoreProps>()((set) => ({
-  model: "gpt-3.5-turbo",
-
-  setModel: (model) => {
-    set((state) => ({ ...state, model: model }));
-  },
-}));
+const useSelectedModelStore = create<UseSelectedModelStoreProps>()(
+  persist(
+    (set) => ({
+      model: "gpt-3.5-turbo",
+      setModel: (model) => {
+        set((state) => ({ ...state, model: model }));
+      },
+    }),
+    {
+      name: "model",
+    }
+  )
+);
 
 export default useSelectedModelStore;
