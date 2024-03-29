1fdac69012709b03fc7a3358c2745c75674cec8f
WOOJINLEEdev <lwj3794@naver.com>
2024-03-29 11:37:09 +0900

    feat: model 그룹핑

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -13,3 +13,21 @@ export const MODELS = {
   MIXTRAL: "mixtral-8x7b-32768",
   GEMINI: "gemini-pro",
 };
+
+interface GroupedModelsByPlatform {
+  [key: string]: { [key: string]: string }[];
+}
+
+export const GROUPED_MODELS_BY_PLATFORM: GroupedModelsByPlatform = {
+  OpenAI: [
+    { id: "1", name: "GPT-3.5", model: "gpt-3.5-turbo" },
+    { id: "2", name: "GPT-4", model: "gpt-4-turbo-preview" },
+    { id: "3", name: "DALL·E 3", model: "dall-e-3" },
+  ],
+  Microsoft: [{ id: "4", name: "Bing", model: "bing" }],
+  Groq: [
+    { id: "5", name: "LLaMA2", model: "llama2-70b-4096" },
+    { id: "6", name: "Mixtral", model: "mixtral-8x7b-32768" },
+  ],
+  Google: [{ id: "7", name: "Gemini", model: "gemini-pro" }],
+};
