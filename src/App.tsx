171e391b95aafeeb711cfe0f9f29a4ae38395426
WOOJINLEEdev <lwj3794@naver.com>
2024-04-04 16:14:17 +0900

    feat: 버튼을 통한 sidebar 열기/닫기 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   useKeyBinding();
 
   return (
-    <div className="container flex items-center justify-center w-full h-full min-h-screen p-0 m-0">
+    <div className="container relative flex w-screen h-full min-h-screen p-0 m-0">
       <Router />
     </div>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,31 @@
 import ScrollToBottom from "react-scroll-to-bottom";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import { TooltipArrow } from "@radix-ui/react-tooltip";
 
 import { useGetMessages } from "@/hooks/db/useGetMessages";
 
 import ChatInput from "@/components/ChatInput";
 import ChatSelect from "@/components/ChatSelect";
 import ChatView from "@/components/ChatView";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 import useConversationStore from "@/state/useConversationStore";
+import useSideBarStore from "@/state/useSidebarStore";
 
 const MainPage = () => {
   const { selectedConversationId } = useConversationStore();
+  const { isOpen, toggle } = useSideBarStore();
 
   const { messages } = useGetMessages({ id: selectedConversationId });
 
   return (
-    <main className="flex flex-col justify-between w-full h-screen p-6">
+    <main className="relative flex flex-col justify-between min-w-full h-screen p-6">
       <div className="flex justify-start py-2">
         <ChatSelect />
       </div>
@@ -33,6 +44,36 @@ const MainPage = () => {
         )}
 
       <ChatInput />
+
+      <div className="absolute top-1/2 left-2 transform -translate-x-1/2 -translate-y-1/2">
+        <TooltipProvider delayDuration={100}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                type="button"
+                variant="ghost"
+                className="px-1 py-3 hover:bg-transparent"
+                onClick={() => toggle()}
+              >
+                {isOpen ? (
+                  <ChevronLeftIcon className="w-5 h-5" />
+                ) : (
+                  <ChevronRightIcon className="w-5 h-5" />
+                )}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent
+              side="right"
+              sideOffset={-5}
+              align="center"
+              className="bg-black border-black text-gray-100"
+            >
+              <p>{isOpen ? "Close sidebar" : "Open sidebar"}</p>
+              <TooltipArrow className="animate-in fade-in-0 zoom-in-95" />
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </main>
   );
 };
