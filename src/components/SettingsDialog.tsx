d52e2bd3fd07a30b333409eb763e1450ef364079
WOOJINLEEdev <79506033+WOOJINLEEdev@users.noreply.github.com>
2024-04-14 23:27:30 +0900

    feat: settings dialog에 버전 표시 (#107)

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+
+import { getVersion } from "@tauri-apps/api/app";
+
 import {
   Dialog,
   DialogContent,
@@ -10,6 +14,17 @@ import useSettingsStore from "@/state/useSettingsStore";
 
 const SettingsDialog = () => {
   const { isOpen, toggle } = useSettingsStore();
+  const [appVersion, setAppVersion] = useState("");
+
+  useEffect(() => {
+    async function getAppVersion() {
+      const appVersion = await getVersion();
+
+      setAppVersion(appVersion);
+    }
+
+    getAppVersion();
+  }, []);
 
   return (
     <Dialog open={isOpen} onOpenChange={toggle}>
@@ -19,13 +34,14 @@ const SettingsDialog = () => {
           event.preventDefault();
         }}
       >
-        <div className="p-6 border-b border-gray-100">
+        <div className="relative p-6 border-b border-gray-100">
           <DialogHeader>
             <DialogTitle>Settings</DialogTitle>
           </DialogHeader>
         </div>
 
         <SettingsLayout />
+        <div className="absolute bottom-3 left-6 text-xs">v{appVersion}</div>
       </DialogContent>
     </Dialog>
   );
